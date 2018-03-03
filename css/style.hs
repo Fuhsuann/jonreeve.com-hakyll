49a8aca7e2fffa9e00b3bf938f54d452e5f84082
Jonathan Reeve <jon.reeve@gmail.com>
2018-03-03 08:31:11 -0500

    migrate to clay, some styling, add mdb

--- css/style.hs
+++ css/style.hs
@@ -4,16 +4,18 @@ import Prelude hiding (div, rem)
 
 test :: Css
 test = do
-  body ?
-    do backgroundColor    blue
-       fontSize           (px 16)
-  div # "#social" ?
-    do display        flex
-       alignItems     center
-       justifyContent flexEnd
-       i ? do
-         fontSize (rem 2)
-         -- margin   (em 1)
+  div # "#content" ? 
+    sym2 padding (rem 2) (rem 2)
+  footer ? do
+    sym padding (rem 3)
+    div # "#social" ?
+      do display        flex
+         alignItems     center
+         justifyContent flexEnd
+         i ? do
+           fontSize (rem 2)
+           -- `sym` is shorthand and allows for `margin 1 1 1 1`
+           sym margin (em 1)
 
 main :: IO ()
 main = putCss test 
